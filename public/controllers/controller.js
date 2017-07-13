3309e4c9eb4df4a80f40ebd1a0f8555b2d2799cb
Kimberli Smith <kimsmith.it@gmail.com>
2017-07-13 00:03:16 -0400

    corrected angular code

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -5,7 +5,7 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 var refresh = function(){
   $http.get('/eventlist/').then(function(response) {
     console.log("I got the data I requested");
-    $scope.eventlist = response;
+    $scope.eventlist = response.data;
     $scope.event = null;
   });
 };
